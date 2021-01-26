096b03ba4ea6323f624cf594d392b7c1a51c6b70
ddang4370 <77597515+ddang4370@users.noreply.github.com>
2021-01-26 14:50:36 -0500

    Add files via upload

---
+++ Assignment_2.R
@@ -0,0 +1,10 @@
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+
+ABC_results <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS_results <- c(12, 75, 43, 19, 1, 21, 19) 
+
+df <- data.frame(Name, ABC_results, CBS_results)
+df
+
+#in the 2016 election (not actual results), we can observe with the dataframe that Donald had majority vote.
